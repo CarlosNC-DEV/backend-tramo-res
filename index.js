5b31d326487145ef0c7a0b7a9d3750cf61a58349
CarlosNC-DEV <carlosnogueradev@gmail.com>
2023-04-01 21:41:51 -0500

    add response rest api

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import app from "./src/app.js";
 import { PORT } from "./src/config.js";
 import "./src/db.js";
 
+app.use((req, res, next) => {
+  return res.status(200).json('Bienvenido a TRAMO')
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
 });
