4b60acb0774ac48d925a2a3a08335319c0509d5b
CarlosNC-DEV <carlosnogueradev@gmail.com>
2023-04-10 17:41:23 -0500

    Token Ciente natura

--- src/controllers/login.natural.controllers.js
+++ src/controllers/login.natural.controllers.js
@@ -25,7 +25,7 @@ export const authClienteNatural = async (req, res) => {
     }
 
     const token = jwt.sign({ id: clienteFound._id }, JWT_SECRET, {
-      expiresIn: 86400,
+      expiresIn: 120,
     });
 
     res.status(200).json({
