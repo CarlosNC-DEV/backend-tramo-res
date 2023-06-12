c7429ad50d4d7681a3194b0e37da71c827b3c562
CarlosNC-DEV <carlosnogueradev@gmail.com>
2023-06-12 13:22:08 -0500

    Add Pqrs Unique And Pqrs Data User

--- src/controllers/pqrs.controllers.js
+++ src/controllers/pqrs.controllers.js
@@ -1,10 +1,34 @@
 import Pqrs from '../models/Pqrs.js';
+import ClienteNatural from '../models/ClienteNatural.js';
+import ClienteEmpresa from '../models/ClienteEmpresa.js';
+import Conductores from '../models/Conductores.js';
 
 
 export const verPqrs = async(req, res)=>{
     try {
+
+        const pqrsFound = [];
+
         const pqrs = await Pqrs.find().lean();
-        res.status(200).json(pqrs);
+        for (const pqrsItem of pqrs) {
+          const usuarioNaturalFound = await ClienteNatural.findById(pqrsItem.id_usuario);
+          if (usuarioNaturalFound) {
+            pqrsFound.push({ pqrsItem, usuario: usuarioNaturalFound });
+          } else {
+            const usuarioEmpresaFound = await ClienteEmpresa.findById(pqrsItem.id_usuario);
+            if (usuarioEmpresaFound) {
+              pqrsFound.push({ pqrsItem, usuario: usuarioEmpresaFound });
+            } else {
+              const usuarioConductorFound = await Conductores.findById(pqrsItem.id_usuario);
+              if (usuarioConductorFound) {
+                pqrsFound.push({ pqrsItem, usuario: usuarioConductorFound });
+              }
+            }
+          }
+        }
+    
+        res.status(200).json(pqrsFound);
+
     } catch (error) {
         console.log(error);
         return res.status(500).json("Error en el sevidor");
@@ -22,6 +46,35 @@ export const crearPqrs = async(req, res)=>{
 
         res.status(200).json("Pqrs Creada");
 
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json("Error en el sevidor");
+    }
+}
+
+export const verUniquePqrs = async(req , res)=>{
+    try {
+        const pqrsUnique = [];
+        const { id } = req.params;
+        const pqrsFound = await Pqrs.findById(id).lean();
+    
+        const usuarioNaturalFound = await ClienteNatural.findById(pqrsFound.id_usuario);
+        if (usuarioNaturalFound) {
+            pqrsUnique.push(pqrsFound, usuarioNaturalFound);
+        } else {
+          const usuarioEmpresaFound = await ClienteEmpresa.findById(pqrsFound.id_usuario);
+          if (usuarioEmpresaFound) {
+            pqrsUnique.push(pqrsFound, usuarioEmpresaFound);
+          } else {
+            const usuarioConductorFound = await Conductores.findById(pqrsFound.id_usuario);
+            if (usuarioConductorFound) {
+                pqrsUnique.push(pqrsFound, usuarioConductorFound);
+            }
+          }
+        }
+    
+        res.status(200).json(pqrsUnique);
+        
     } catch (error) {
         console.log(error);
         return res.status(500).json("Error en el sevidor");
--- src/routes/pqrs.routes.js
+++ src/routes/pqrs.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { verPqrs, crearPqrs } from '../controllers/pqrs.controllers.js';
+import { verPqrs, crearPqrs, verUniquePqrs } from '../controllers/pqrs.controllers.js';
 
 const router = Router();
 
 router.get("/pqrs", verPqrs)
+router.get("/pqrs/:id", verUniquePqrs)
 router.post("/pqrs", crearPqrs)
 
 export default router;
